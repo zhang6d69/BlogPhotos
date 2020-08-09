fb379ba5d908e4f94712acd6a1a5d4c3635b9d93
zhang6d69 <2824742450@qq.com>
2020-08-10 01:13:26 +0800

    add photos

--- tool.py
+++ tool.py
@@ -117,8 +117,9 @@ def handle_photo():
                                         } 
             list_info.append(new_dict)
         elif year_month != list_info[-1]['date']:  # 不是最后的一个日期，就新建一个dict
-            new_dict = {"date": year_month, "arr":{'year': date.year,
+            new_dict = {"date": year_month_day, "arr":{'year': date.year,
                                                    'month': date.month,
+												  'day': date.day,
                                                    'link': [filename],
                                                    'text': [info],
                                                    'type': ['image']
